6a31cc649f0487c46373ce2405d3a3aabc2eec9d
AndreiRaduStoian <andreiradu.stoian@gmail.com>
2025-01-18 15:33:05 +0100

    cleanup

--- visualize.py
+++ visualize.py
@@ -9,43 +9,34 @@ matplotlib.use('Qt5Agg')
 plt.ion()
 
 
-# Function to update scatter plot in real time
 def update_plot(coords):
     scatters._offsets3d = (
         coords[:, 0],  # X-coordinates
         coords[:, 1],  # Y-coordinates
         coords[:, 2],  # Z-coordinates
     )
-    plt.draw()  # Redraw the updated plot
-    plt.pause(0.01)  # Pause to allow real-time update
+    plt.draw()
+    plt.pause(0.005)
 
 
-# Initialize the 3D plot
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-
-
-# Set axis limits (adjust as needed)
-ax.set_xlim(0, 30)
-ax.set_ylim(0, 30)
-ax.set_zlim(0, 30)
-
-
-# Create scatter plot (one scatter point per planet)
+ax.set_xlim(0, 20)
+ax.set_ylim(0, 20)
+ax.set_zlim(0, 20)
 scatters = ax.scatter([], [], [], s=50)
 
 
 plt.show()
 
 
-points = np.array([[1, 3, 1], [5, 5, 3], [10, 10, 7]], dtype=np.float64)
+points = np.array([[1, 3, 1], [5, 5, 3], [1, 5, 7]], dtype=np.float64)
 velocities = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], dtype=np.float64)
-masses = np.array([10, 1, 4], dtype=np.float64)
+masses = np.array([0.5, 0.5, 0.5], dtype=np.float64)
 
 
 dt = 0.1
-tts = 100
-
+tts = 400
 try:
     update_plot(points)
     for step in range(tts):
