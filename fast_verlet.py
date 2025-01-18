638469b2edca4d3a7d7366c2a5e9da8799d1cda3
AndreiRaduStoian <andreiradu.stoian@gmail.com>
2025-01-18 16:52:14 +0100

    tested optimised verlet

--- fast_verlet.py
+++ fast_verlet.py
@@ -1,9 +1,11 @@
 import numpy as np
+import main
 
 
 points = np.random.uniform(-100, 100, size=(10, 3))
 vels = np.random.uniform(-10, 10, size=(10, 3))
 masses = np.random.uniform(1, 100, size=10)
+print(masses[np.newaxis, :, np.newaxis])
 
 
 def pairwise_distances(pos):
@@ -12,23 +14,28 @@ def pairwise_distances(pos):
     return distances, diff
 
 
-dist, diff = pairwise_distances(points)
-print(diff[0])
-diff2 = np.zeros_like(diff[0])
-for i in range(len(points)):
-    diff2[i] = points[0] - points[i]
-print(diff2)
+# dist, diff = pairwise_distances(points)
+# print(diff[0])
+# diff2 = np.zeros_like(diff[0])
+# for i in range(len(points)):
+#     diff2[i] = points[0] - points[i]
+# print(diff2)
 
 
 def pairwise_forces(pos, masses, G=1.0):
-    """
-    Compute forces on each body due to all others.
-    """
     distances, diff = pairwise_distances(pos)
     distances_inv3 = np.where(distances > 0, distances**-3, 0)
-    forces = G * diff * (distances_inv3[:, :, np.newaxis] * masses[np.newaxis, :, np.newaxis])
+    forces = G * diff * (distances_inv3[:, :, np.newaxis] * (masses[:, np.newaxis] * masses[np.newaxis, :])[:, :, np.newaxis])
     net_forces = np.sum(forces, axis=1)
     return net_forces
+
+
+pw_f = pairwise_forces(points, masses)
+print(pw_f[0])
+print()
+
+f = main.force_all(0, points, masses)
+print(f)
 
 
 def fast_verlet_update(pos, vel, masses, dt, G=1.0):
